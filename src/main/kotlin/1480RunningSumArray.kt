008c30c539dbdc8a50f09fa567e7fc66d5cc59a8
OksanaAlexandrovna <ksushka1960@mail.ru>
2023-12-02 18:46:16 +0300

    Add a solution and new unit test

--- src/main/kotlin/1480RunningSumArray.kt
+++ src/main/kotlin/1480RunningSumArray.kt
@@ -1,10 +1,13 @@
 val scan = java.util.Scanner(System.`in`)
 fun main(args: Array<String>) {
-    println(runningSum(intArrayOf(1, 2, 3, 4)).contentToString())
+    var sumRanning = RunningSum()
+    println(sumRanning.runningSum(intArrayOf(1,2,3,4)).contentToString())
 }
-fun runningSum(nums: IntArray): IntArray {
-    for (i in 1 until nums.size) {
-        nums[i] = nums[i] + nums[i-1]
+class RunningSum{
+    fun runningSum(nums: IntArray): IntArray {
+        for (i in 1 until nums.size) {
+            nums[i] = nums[i] + nums[i-1]
+        }
+        return nums
     }
-    return nums
 }
--- src/main/kotlin/771JewelsAndStones.kt
+++ src/main/kotlin/771JewelsAndStones.kt
@@ -1,15 +1,18 @@
 val scanner = java.util.Scanner(System.`in`)
 fun main(args: Array<String>) {
-    println(numJewelsInStones("aA", "aAAbbbb"))
+    var jewelsStone = JewelsStone()
+    println(jewelsStone.numJewelsInStones("aA", "aAAbbbb"))
 }
-fun numJewelsInStones(jewels: String, stones: String): Int {
-    var num = 0
-    for (i in stones) {
-        for (j in jewels) {
-            if (i == j) {
-                num += 1
+class JewelsStone{
+    fun numJewelsInStones(jewels: String, stones: String): Int {
+        var num = 0
+        for (i in stones) {
+            for (j in jewels) {
+                if (i == j) {
+                    num += 1
+                }
             }
         }
+        return num
     }
-    return num
 }
